863b6bbc71f682550feaf04918468bc4532b55ed
菅原睦 <sugawara@sugawaraatsushinoMacBook-Air.local>
2023-07-03 12:20:33 +0900

    first commit

---
+++ containers/entrypoint.sh
@@ -0,0 +1,10 @@
+# containers/entrypoint.sh
+
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
